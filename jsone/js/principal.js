0a6923ab026a7e6fd83887d7ae76fdc83b5a91d3
Bruno Lima <bruno-amil@hotmail.com>
2020-01-20 23:49:11 -0300

    mudando o background de acordo com o estado

--- jsone/js/principal.js
+++ jsone/js/principal.js
@@ -2,34 +2,44 @@ var nome = document.querySelector(".titulo");
 nome.textContent = "Bruno Lima";
 
 //selecionando os pacientes
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
+   // console.log(pacientes);
+   
+    //loop for
+    for(var i = 0; i < pacientes.length; i++){
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+        var paciente = pacientes[i];
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+        var tdPeso = paciente.querySelector(".info-peso");
+        var peso = tdPeso.textContent;
 
-var tdImc = paciente.querySelector(".info-imc");
+        var tdAltura = paciente.querySelector(".info-altura");
+        var altura = tdAltura.textContent;
 
-var pesoEhValido = true;
-var alturaEhValida = true;
+        var tdImc = paciente.querySelector(".info-imc");
 
-if (peso <= 0 || peso >= 1000) {
-    console.log("Peso invalido");
-    pesoEhValido = false;
-    tdImc.textContent = "Peso invalido";
-}
+        var pesoEhValido = true;
+        var alturaEhValida = true;
 
-if (altura <= 0 || altura >= 3.00) {
-    console.log("Altura invalida");
-    alturaEhValida = false;
-    tdImc.textContent = "Altura invalida";
-}
+        if (peso <= 0 || peso >= 1000) {
+            console.log("Peso invalido");
+            pesoEhValido = false;
+            tdImc.textContent = "Peso invalido";
+            paciente.classList.add('paciente-invalido');
+        }
 
-if (alturaEhValida && pesoEhValido) {
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
-}
+        if (altura <= 0 || altura >= 3.00) {
+            console.log("Altura invalida");
+            alturaEhValida = false;
+            tdImc.textContent = "Altura invalida";
+            paciente.classList.add('paciente-invalido');
+        }
+
+        if (alturaEhValida && pesoEhValido) {
+            var imc = peso / (altura * altura);
+            tdImc.textContent = imc.toFixed(2);
+        }
+    }
+        
 
 
