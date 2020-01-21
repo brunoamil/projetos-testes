996a9fb5de690a5eb17166416303ba2b442e00c3
Bruno Lima <bruno-amil@hotmail.com>
2020-01-20 23:11:39 -0300

    pegando os pacientes e calculando o imc

--- jsone/js/principal.js
+++ jsone/js/principal.js
@@ -1,17 +1,35 @@
 var nome = document.querySelector(".titulo");
 nome.textContent = "Bruno Lima";
 
-//selecionando o paciente
-
+//selecionando os pacientes
 var paciente = document.querySelector("#primeiro-paciente");
+
 var tdPeso = paciente.querySelector(".info-peso");
 var peso = tdPeso.textContent;
 
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
-
 var tdImc = paciente.querySelector(".info-imc");
-var imc = peso / (altura * altura);
 
-tdImc.textContent = imc;
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if (peso <= 0 || peso >= 1000) {
+    console.log("Peso invalido");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso invalido";
+}
+
+if (altura <= 0 || altura >= 3.00) {
+    console.log("Altura invalida");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura invalida";
+}
+
+if (alturaEhValida && pesoEhValido) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
+
+
